a170a2d5e6182a0ce85e4858adab113243820740
Quentin Fonck <quentin.fonck@internet.lu>
2016-12-28 14:43:05 +0100

    creating the about page

--- app/routes.js
+++ app/routes.js
@@ -4,16 +4,29 @@ var path = require('path');
 // create our router object
 var router = express.Router();
 
-// export our router
-module.exports = router;
 //route our
 router.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.render("pages/index");
 });
 
 router.get('/about', function(req, res){
-  res.send('Coucou About me!');
+
+  var users = [
+    {name: "Holly", email: "holly@mail.com", avatar: "http://placekitten.com/700/700"},
+    {name: "Patrick", email: "patrick@mail.com", avatar: "http://placekitten.com/700/700"},
+    {name: "Bob", email: "bob@mail.com", avatar: "http://placekitten.com/700/700"},
+    {name: "Jesus", email: "jesus@mail.com", avatar: "http://placekitten.com/700/700"}
+  ];
+  res.render("pages/about", {users : users});
 });
 
-router.get('/contact');
-router.post('/contact');
+router.get('/contact', function(req, res){
+  res.render("pages/contact");
+});
+
+router.post("/contact", function(req, res){
+
+});
+
+// export our router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 // require our dependencies
 var express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 var app = express();
 var port = 8080;
+
+// use ejs and express Layouts
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 //route our app
 var router = require('./app/routes');
 app.use('/', router);
+
 
 //set static files (css and images, ect) location
 app.use(express.static(__dirname+'/public'));
 
 //start server
 app.listen(port, function(){
-  console.log('app started');
+  console.log(`app started on ${port}`);
 });
